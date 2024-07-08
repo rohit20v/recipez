a8b7756f8ac0260f1ea6718df3cef360deb78970
Rohit <rohit.verma@itsrizzoli.it>
2024-07-08 23:10:36 +0200

    Added home component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {SignInComponent} from "./sign-in/sign-in.component";
+import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "sign-in", component: SignInComponent},
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: "home", component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
