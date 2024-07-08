3503f87cc02f7e03cb3c23d6f68bc883c4ba2158
Rohit <rohit.verma@itsrizzoli.it>
2024-07-08 23:09:45 +0200

    Added login endpoint

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,6 +1,5 @@
 import {PrismaClient} from "@prisma/client";
 import express from "express";
-import userRouter from "../routers/user.router";
 
 const userClient = new PrismaClient().user
 
@@ -20,25 +19,26 @@ export const getAllUsers = async (req: express.Request, res: express.Response) =
     }
 }
 
-//get user by id
-export const getUserById = async (req: express.Request, res: express.Response) => {
+// login user
+export const loginUser = async (req: express.Request, res: express.Response) => {
     try {
-        const userId = req.params.id;
+        const {email, password} = req.body;
         const user = await userClient.findUnique({
             where: {
-                id: parseInt(userId)
-            },
-            include: {
-                recipes: true
+                email: email
             }
         });
-
-        res.status(200).json({data: user})
-
+        if (user && user.password === password) {
+            res.status(200).json({message: 'Login successful', user});
+        } else {
+            res.status(401).json({message: 'Invalid email or password'});
+        }
     } catch (err) {
-        console.log(err)
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
     }
-}
+};
+
 //create user
 export const createUser = async (req: express.Request, res: express.Response) => {
     try {
@@ -46,7 +46,7 @@ export const createUser = async (req: express.Request, res: express.Response) =>
         const userBody = req.body
         console.log('Request Body:', req.body);
 
-        const { email, password } = req.body;
+        const {email, password} = req.body;
         if (!email || !password) {
             return res.status(400).send('Email and password are required');
         }
@@ -66,7 +66,7 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
     try {
         const userId = req.params.id
         const user = await userClient.delete({
-            where:{
+            where: {
                 id: parseInt(userId)
             }
         });
--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,11 +1,12 @@
 import {Router} from "express";
-import {createUser, deleteUser, getAllUsers, getUserById} from "../controllers/user.controller";
+import {createUser, deleteUser, getAllUsers, loginUser} from "../controllers/user.controller";
 
 const userRouter = Router()
 
 userRouter.get('/', getAllUsers)
-userRouter.get('/:id', getUserById)
 userRouter.post('/', createUser)
 userRouter.get('/:id', deleteUser)
+userRouter.post('/login', loginUser);
+
 
 export default userRouter
