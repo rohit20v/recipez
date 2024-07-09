b70b3e0c2d6d8c5f8293393750a15e85dec764d7
Rohit <rohit.verma@itsrizzoli.it>
2024-07-09 22:56:41 +0200

    Update home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {RecipeComponent} from "../recipe/recipe.component";
+import {CommonModule} from "@angular/common";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RecipeComponent,
+    CommonModule,
+    HttpClientModule,
+  ],
   templateUrl: './home.component.html',
+  // template: `<div>h</div>`,
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  recipes?: RecipeComponent[]
+  recipeSub?: Subscription;
 
+  constructor(private http: HttpClient,) {
+  }
+
+  ngOnInit() {
+    this.recipeSub = this.http.get('http://localhost:3000/recipe/all').subscribe(
+      (res: any) => {
+        this.recipes = res.data
+        // console.log(res.data)
+      },
+      err => {
+        console.log(err)
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    if (this.recipeSub) {
+      this.recipeSub.unsubscribe();
+    }
+  }
 }
