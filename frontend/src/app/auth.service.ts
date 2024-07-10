4e4cff92e22882357a75ad1356143be9fb763360
Rohit <rohit.verma@itsrizzoli.it>
2024-07-10 21:54:06 +0200

    Added getUserId method

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -9,15 +9,23 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) {
   }
 
+
   login(email: string, password: string) {
-    return this.http.post('http://localhost:3000/user/login', {email, password}).subscribe(
-      user => {
+    return this.http.post('http://localhost:3000/user/login', {email, password}).subscribe({
+      next: user => {
         localStorage.setItem('currentUser', JSON.stringify(user));
         this.router.navigate(['/']);
       },
-      error => {
+      error: error => {
         throw error
-      });
+      }
+    })
+  }
+
+  get getCurrentUserId() {
+    const currentUser = localStorage.getItem('currentUser');
+    return currentUser ? JSON.parse(currentUser).user.id : null;
+
   }
 
   logout() {
