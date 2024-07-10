bb3b7fc6beb098448f6a7049022cbe87ac402d56
Rohit <rohit.verma@itsrizzoli.it>
2024-07-10 21:53:02 +0200

    Updated app-recipe component

--- frontend/src/app/add-recipe/add-recipe.component.ts
+++ frontend/src/app/add-recipe/add-recipe.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "../auth.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-add-recipe',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './add-recipe.component.html',
   styleUrl: './add-recipe.component.scss'
@@ -18,10 +20,38 @@ export class AddRecipeComponent {
     name: new FormControl("", [Validators.required]),
     description: new FormControl("", [Validators.required]),
     ingredients: new FormControl("", [Validators.required]),
-    instruction: new FormControl("", [Validators.required]),
+    instructions: new FormControl("", [Validators.required]),
     image: new FormControl(""),
   })
 
+  authService = inject(AuthService);
+
+  constructor(private router: Router, private http: HttpClient){
+  }
+
   addNewRecipe() {
+    if (this.formData.valid) {
+      if (this.authService?.isLoggedIn) {
+        const recipe = {
+          userId: this.authService.getCurrentUserId,
+          name: this.formData.value.name,
+          description: this.formData.value.description,
+          ingredients: this.formData.value.ingredients,
+          instructions: this.formData.value.instructions,
+          image: this.formData.value.image
+        };
+        console.log("recipe", recipe)
+        this.http.post("http://localhost:3000/recipe/new", recipe).subscribe(
+          {
+            next: () => {
+              this.router.navigate(['/'])
+            },
+            error: (err) => {
+              console.log(err)
+            }
+          }
+        )
+      }
+    }
   }
 }
