679ff2faaab1e7dc9423ce54aa768234a27aaaed
Rohit <rohit.verma@itsrizzoli.it>
2024-07-10 21:53:39 +0200

    Updated routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import {LoginComponent} from "./login/login.component";
 import {SignInComponent} from "./sign-in/sign-in.component";
 import {HomeComponent} from "./home/home.component";
 import {AddRecipeComponent} from "./add-recipe/add-recipe.component";
+import {AuthGuard} from "./auth.guard.guard";
 
 export const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "sign-in", component: SignInComponent},
   {path: "home", component: HomeComponent},
-  {path: "add-recipe", component: AddRecipeComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: "add-recipe", component: AddRecipeComponent, canActivate:[AuthGuard]},
+  { path: '' || "**", redirectTo: '/home', pathMatch: 'full' },
 ];
