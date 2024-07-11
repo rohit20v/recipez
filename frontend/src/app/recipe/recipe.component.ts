e5e07e4171fce6dd9c3c5f286a11991c81f60c42
Rohit <rohit.verma@itsrizzoli.it>
2024-07-11 18:28:19 +0200

    Added fav recipe

--- frontend/src/app/recipe/recipe.component.ts
+++ frontend/src/app/recipe/recipe.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {Component, EventEmitter, inject, Input, OnInit, Output} from '@angular/core';
 import {NgIf} from "@angular/common";
 import {AuthService} from "../auth.service";
 import {HttpClient} from "@angular/common/http";
@@ -9,15 +9,16 @@ import {environment} from "../../environments/environment";
   selector: 'app-recipe',
   standalone: true,
   imports: [
-    NgIf
+    NgIf,
   ],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.scss'
 })
-export class RecipeComponent {
+export class RecipeComponent implements OnInit {
 
   authService = inject(AuthService);
   @Input() userId?: number
+  @Input() recipeUserId?: number
   @Input() id?: number
   @Input() name = "Default name"
   @Input() ingredients = "Default ingredients"
@@ -25,13 +26,38 @@ export class RecipeComponent {
   @Input() description = "Default description"
   @Output() recipeRemoved = new EventEmitter<number>();
 
+
+  fav?: boolean
+
   http = inject(HttpClient);
   httpSub?: Subscription
+
+  ngOnInit() {
+    console.log("this.userId", this.userId)
+    console.log("this.recipeUserId", this.recipeUserId)
+  }
 
   removeRecipe() {
     this.httpSub = this.http.delete(`${environment.eliminationUrl}${(this.id)}`).subscribe({
       next: () => {
         this.recipeRemoved.emit(this.id);
+      },
+      error: err =>
+        console.log(err)
+    })
+  }
+
+  updateFav() {
+    this.fav = !this.fav
+
+    const body = {
+      isFav: this.fav,
+      userId: this.userId,
+      recipeId: this.id
+    }
+    this.http.put("http://localhost:3000/fav", body).subscribe({
+      next: (res) => {
+        console.log(res)
       },
       error: err =>
         console.log(err)
